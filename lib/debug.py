16c7863f30fed92bcdab8da5351e6de50751f01a
Kalvine <kkalvine4@gmail.com>
2024-09-10 20:27:14 +0300

    maps and deletes data from table

--- lib/debug.py
+++ lib/debug.py
@@ -6,4 +6,26 @@ from department import Department
 
 import ipdb
 
+Department.drop_table()
+Department.create_table()
+
+payroll = Department.create("Payroll", "Building A, 5th Floor")
+print(payroll)
+
+hr = Department.create("Human Resources", "Building C, East Wing")
+print(hr)
+
+accounting = Department.create("Accounting", "Building C, East Wing")
+print(accounting)
+
+hr.name = "HR"
+hr.location = "Building F, 10th Floor"
+hr.update()
+print(hr)
+
+print("Delete Accounting")
+accounting.delete()
+print(accounting)
+
+
 ipdb.set_trace()
